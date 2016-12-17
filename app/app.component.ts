feb562cd0c266aa6f1bd93d9f4588f484ebf1d35
Rafael Azevedo <raf.azevedo@gmail.com>
2016-12-17 16:39:14 -0200

    Revert "Aulas 01-06"
    This reverts commit 6abb68e35c55b47a9a346a626db6d9b2b46830c9.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 
-import { PrimeiroComponent } from './primeiro/primeiro.component';
-import { CursosComponent } from './cursos/cursos.component';
-
 @Component({
     selector: 'my-app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
-        <p>Oi <b>MEU <span style="color:red;">BEM!</span></b></p>
-        <primeiro></primeiro>
-        <cursos-lista></cursos-lista>
-    `,
-    directives: [PrimeiroComponent, CursosComponent]
+        <p>Hello World!</p>
+    `
 })
 export class AppComponent { }
